92f298ae870a72bb2619b3f65da9b106eb9f5dd9
yaches <@>
2017-03-15 12:55:27 +0300

    update database config

--- dbAPI/dbAPI/settings.py
+++ dbAPI/dbAPI/settings.py
@@ -77,10 +77,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dbapi',
-        'USER' : 'postgres',
-        # 'PASSWORD' : 'password',
-        # 'HOST' : '127.0.0.1',
-        # 'PORT' : '5432',
+        'USER' : 'postgres'
     },
 }
 
